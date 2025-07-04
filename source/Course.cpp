89d2d1784970e80a929550c7db91ee7a91a395f9
toproof <kmnlmn123@gmail.com>
2025-07-04 14:11:23 +0900

    ✨ Feat: Course.cpp에서 과목 구조체 데이터를 수정하는 함수 추가

--- source/Course.cpp
+++ source/Course.cpp
@@ -3,72 +3,100 @@
 #include <iostream>
 #include "Course.h"
 
-// 정보 출력
-void printCourse(const Course& c)
+namespace Course 
 {
-  std::cout << "  - " << std::left << std::setw(50) << c.courseName // 과목명 (20칸 좌측 정렬)
-            << " | 학점: " << std::setw(10) << c.credits       // 이수학점 (2칸)
-            << " | 점수: " << std::setw(10) << convertToGrade(c.grade) // 점수 (4칸)
-            << " | 분류: " << std::left << std::setw(20) << convertToCategory(c.category) // 분류 (10칸 좌측 정렬)
-            << std::endl;
-}
 
-std::string convertToGrade(int grade)
-{
-  std::string g[9] = {"A+", "A", "B+", "B", "C+", "C", "D+", "D", "F"};
-  int index = 0;
-
-  for (double i = 4.5; i>=0.5; i-=0.5)
+  // Course 정보 출력
+  void printCourse(const Course& c)
   {
-    if (grade >= i) 
+    std::cout << "  - " << std::left << std::setw(50) << c.courseName // 과목명 (20칸 좌측 정렬)
+              << " | 학점: " << std::setw(10) << c.credits       // 이수학점 (2칸)
+              << " | 점수: " << std::setw(10) << convertToGrade(c.grade) // 점수 (4칸)
+              << " | 분류: " << std::left << std::setw(20) << convertToCategory(c.category) // 분류 (10칸 좌측 정렬)
+              << std::endl;
+  }
+
+  std::string convertToGrade(const int& grade)
+  {
+    std::string g[9] = {"A+", "A", "B+", "B", "C+", "C", "D+", "D", "F"};
+    int index = 0;
+
+    for (double i = 4.5; i>=0.5; i-=0.5)
     {
-      return g[index];
+      if (grade >= i) 
+      {
+        return g[index];
+      }
+      index++;
     }
-    index++;
+    return "X";
   }
-  return "X";
-}
-
-std::string convertToCategory(int category)
-{
-  switch (category)
+  std::string convertToCategory(const int& category)
   {
-  case MajorRequired:
-    return "전공선택";
-    break;
+    switch (category)
+    {
+    case MajorRequired:
+      return "전공선택";
+      break;
 
-  case DoubleMajor:
-    return "복수전공";
-    break;
+    case DoubleMajor:
+      return "복수전공";
+      break;
 
-  case Minor:
-    return "부전공";
-    break;
+    case Minor:
+      return "부전공";
+      break;
 
-  case Division:
-    return "계열";
-    break;
+    case Division:
+      return "계열";
+      break;
 
-  case GeneralElective:
-    return "교양";
-    break;
+    case GeneralElective:
+      return "교양";
+      break;
 
-  case Elective:
-    return "자선";
-    break;
+    case Elective:
+      return "자선";
+      break;
 
-  case OtherMajor:
-    return "타전공";
-    break;
-  
-  default:
-    return "ERROR";
-    break;
+    case OtherMajor:
+      return "타전공";
+      break;
+    
+    default:
+      return "ERROR";
+      break;
+    }
   }
-}
 
-// 정렬을 위한 사용자 정의 규칙
-bool courseCompare(const Course& lhs, const Course& rhs)
-{
-  return lhs.grade < rhs.grade;
+  /* 
+  // template를 이용하여 다양한 자료형에 대한 값 처리 (이외에는 오버로딩 혹은 각 value마다 변경 함수를 제작하여 사용할 수 있을듯)
+  template <typename T>
+  void fixCourseValue(Course& c, int choiceFixValue, T& value)
+  {
+      if (choiceFixValue == 1)
+        c.courseName = value;
+      else if (choiceFixValue == 2)
+        c.credits = value;
+      else if (choiceFixValue == 3)
+        c.grade = value;
+      else if (choiceFixValue == 4)
+        c.category = value;
+      else
+        std::cout << "잘못입력했습니다..." << std::endl;
+  }
+  */
+
+  // 각 데이터를 변경하는 setter함수
+  void setCourseName(Course& c, const std::string& name) { c.courseName = name; }
+  void setCredits(Course& c, int credits) { c.credits = credits; }
+  void setGrade(Course& c, double grade) { c.grade = grade; }
+  void setCategory(Course& c, int category) { c.category = category; }
+
+  // 정렬을 위한 사용자 정의 규칙
+  bool courseCompare(const Course& lhs, const Course& rhs)
+  {
+    return lhs.grade < rhs.grade;
+  }
+
 }
--- source/Course.h
+++ source/Course.h
@@ -1,38 +1,55 @@
 #pragma once // 헤더 중복되지 않게 하는 설정
 #include <string>
 
-// 전공 분류
-enum MajorClassification
+namespace Course 
 {
-  MajorRequired   = 0,  // 전선 (전공 필수 - Major Required)
-  DoubleMajor     = 1,  // 복전 (복수 전공 - Double Major)
-  Minor           = 2,  // 부전 (부전공 - Minor)
-  Division        = 3,  // 계열 (계열 필수/선택 - Division)
-  GeneralElective = 4,  // 교양 (교양 - General Elective)
-  Elective        = 5,  // 자선 (자유 선택 - Elective)
-  OtherMajor      = 6   // 타전 (타 전공 - Other Major)
-};
 
-/* ---------- 과목 구조체, 사용자 정의 규칙 정렬, 숫자점수->문자점수 변환 ---------- */
-
-// Course는 과목에 대한 정보를 관리하는 책임을 진다 (저장, 제공)
-struct Course
-{
-  std::string courseName; // 컴퓨터구조
-  int credits;            // 3 (학점)
-  double grade;           // 4.5 (A+)
-  int category;           // 0 (전공선택)
-
-	bool operator<(const Course& otherCourse) const 
+  // 전공 분류
+  enum MajorClassification
   {
-	    return grade < otherCourse.grade;
-	}
-};
+    MajorRequired   = 0,  // 전선 (전공 필수 - Major Required)
+    DoubleMajor     = 1,  // 복전 (복수 전공 - Double Major)
+    Minor           = 2,  // 부전 (부전공 - Minor)
+    Division        = 3,  // 계열 (계열 필수/선택 - Division)
+    GeneralElective = 4,  // 교양 (교양 - General Elective)
+    Elective        = 5,  // 자선 (자유 선택 - Elective)
+    OtherMajor      = 6   // 타전 (타 전공 - Other Major)
+  };
 
+  /* ---------- 과목 구조체, 사용자 정의 규칙 정렬, 숫자점수->문자점수 변환 ---------- */
 
-void printCourse(const Course& c);
-std::string convertToGrade(int grade);
-std::string convertToCategory(int category);
-bool courseCompare(const Course& lhs, const Course& rhs);
+  // Course는 과목에 대한 정보를 관리하는 책임을 진다 (저장)
+  struct Course
+  {
+    std::string courseName; // 컴퓨터구조
+    int credits;            // 3 (학점)
+    double grade;           // 4.5 (A+)
+    int category;           // 0 (전공선택)
 
+    bool operator<(const Course& otherCourse) const 
+    {
+        return grade < otherCourse.grade;
+    }
+  };
 
+  // Course 구조체를 참조하여 정보를 출력
+  void printCourse(const Course& c);
+
+  // 점수와 전공분류를 적절한 문자열로 반환
+  std::string convertToGrade(const int& grade);
+  std::string convertToCategory(const int& category);
+
+  // Course 객체의 변수값을 변경
+  // template는 과목명(string), 점수(double), 전공분류와 학점(int)
+  //template <typename T>
+  //void fixCourseValue(Course& c, int choiceFixValue, T& value);
+
+  // template가 오류가 발생하여 각 setter 함수를 제작
+  void setCourseName(Course& c, const std::string& name);
+  void setCredits(Course& c, int credits);
+  void setGrade(Course& c, double grade);
+  void setCategory(Course& c, int category);
+
+  bool courseCompare(const Course& lhs, const Course& rhs);
+
+}
