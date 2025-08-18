b7519304b19711ca92c25ee13fccadae735ea597
toproof <kmnlmn123@gmail.com>
2025-08-18 16:08:51 +0900

    refactor: 프로그램 관리 클래스 라이브러리 변경

--- src/grade_manager.cpp
+++ src/grade_manager.cpp
@@ -1,15 +1,9 @@
-#include "GradeManager.h"     // 컨트롤러
-#include "App.h"
+#include "Application.h" 
 
 int main()
 {
-  //GradeManager gm;
-  //gm.run();
-
-  GradeApp ga;
-  ga.start();
-
-  return 0;
+  Application app;
+  app.start();
 }
 
 
